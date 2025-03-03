4f69611de2d4a643c705ee6adc06ec2fd5fed6ab
michaljarnot <m.jarnot@yahoo.com>
2025-03-04 00:00:36 +0100

    feat: base app config

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  cnbApiUrl: import.meta.env.CNB_API_URL || 'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing',
+
+  appName: 'CZK Currency Converter',
+
+  defaultCurrency: 'USD'
+};
